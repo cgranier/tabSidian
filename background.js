a7abe1b7222ddf3d0613c73ff51a2b05a90bf08f
Carlos Granier <granier@gmail.com>
2023-04-19 20:42:53 -0400

    V 1.0

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function generateMarkdownAndTimestamp(tabs) {
     const timestamp = new Date();
     const formattedTimestamp = `${timestamp.getFullYear()}-${String(timestamp.getMonth() + 1).padStart(2, '0')}-${String(timestamp.getDate()).padStart(2, '0')}_${String(timestamp.getHours()).padStart(2, '0')}-${String(timestamp.getMinutes()).padStart(2, '0')}-${String(timestamp.getSeconds()).padStart(2, '0')}`;
   
-    let markdown = `# ${formattedTimestamp} Open Tabs\n\n`;
+    let markdown = '';
   
     for (const tab of tabs) {
       markdown += `## ${tab.title}\n[${tab.url}](${tab.url})\n\n`;
@@ -10,7 +10,6 @@ function generateMarkdownAndTimestamp(tabs) {
   
     return { markdown, formattedTimestamp };
   }
-  
   
 
 function shouldProcessTab(tab, restrictedUrls) {
@@ -21,7 +20,7 @@ function shouldProcessTab(tab, restrictedUrls) {
     return !isPinned && !isSettingsTab && !isRestrictedUrl;
   }
   
-chrome.browserAction.onClicked.addListener(() => {
+  chrome.browserAction.onClicked.addListener(() => {
     chrome.storage.sync.get('restrictedUrls', ({ restrictedUrls }) => {
       chrome.windows.getAll({ populate: true, windowTypes: ['normal'] }, (windows) => {
         const currentWindow = windows.find((window) => window.focused);
@@ -30,7 +29,7 @@ chrome.browserAction.onClicked.addListener(() => {
         const { markdown, formattedTimestamp } = generateMarkdownAndTimestamp(tabs);
   
         chrome.downloads.download({
-          url: 'data:text/plain;charset=UTF-8,' + encodeURIComponent(markdown),
+          url: 'data:text/markdown;charset=UTF-8,' + encodeURIComponent(markdown),
           filename: `${formattedTimestamp}_OpenTabs.md`,
           saveAs: true,
         });

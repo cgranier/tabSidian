a0e7a362267ea2eb15e12a62786dec2ce8b06790
Carlos Granier <granier@gmail.com>
2023-04-24 21:10:23 -0400

    V 1.5 Custom Markdown Options

--- background.js
+++ background.js
@@ -1,28 +1,32 @@
-function generateMarkdownAndTimestamp(tabs) {
-    const timestamp = new Date();
-    const formattedTimestamp = `${timestamp.getFullYear()}-${String(timestamp.getMonth() + 1).padStart(2, '0')}-${String(timestamp.getDate()).padStart(2, '0')}T${String(timestamp.getHours()).padStart(2, '0')}-${String(timestamp.getMinutes()).padStart(2, '0')}-${String(timestamp.getSeconds()).padStart(2, '0')}`;
-  
-    const localDate = timestamp.toLocaleString('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    }).replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$1-$2');
-  
-    const localTime = timestamp.toLocaleString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: false,
-    });
-  
-    let markdown = `---\ndate_created: ${localDate}\ntime_created: ${localTime}\n---\n\n`;
-  
-    for (const tab of tabs) {
-      markdown += `## ${tab.title}\n[${tab.url}](${tab.url})\n\n`;
-    }
-  
-    return { markdown, formattedTimestamp };
+function generateMarkdownAndTimestamp(tabs, titleFormat, urlFormat) {
+  const timestamp = new Date();
+  const formattedTimestamp = `${timestamp.getFullYear()}-${String(timestamp.getMonth() + 1).padStart(2, '0')}-${String(timestamp.getDate()).padStart(2, '0')}T${String(timestamp.getHours()).padStart(2, '0')}-${String(timestamp.getMinutes()).padStart(2, '0')}-${String(timestamp.getSeconds()).padStart(2, '0')}`;
+
+  const localDate = timestamp.toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$1-$2');
+
+  const localTime = timestamp.toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  });
+
+  let markdown = `---\ndate_created: ${localDate}\ntime_created: ${localTime}\n---\n\n`;
+
+  for (const tab of tabs) {
+    let formattedTitle = titleFormat.replace(/\{title\}/g, tab.title).replace(/\{url\}/g, tab.url).replace(/\\n/g, '\n');
+    let formattedUrl = urlFormat.replace(/\{title\}/g, tab.title).replace(/\{url\}/g, tab.url).replace(/\\n/g, '\n');
+    
+    markdown += formattedTitle + '\n';
+    markdown += formattedUrl + '\n\n';
   }
+ 
+  return { markdown, formattedTimestamp };
+}
 
   function shouldProcessTab(tab, restrictedUrls, processOnlySelectedTabs) {
     const isPinned = tab.pinned;
@@ -38,17 +42,21 @@ function generateMarkdownAndTimestamp(tabs) {
   }
   
   chrome.browserAction.onClicked.addListener(() => {
-    chrome.storage.sync.get('restrictedUrls', ({ restrictedUrls }) => {
+    chrome.storage.sync.get(['restrictedUrls', 'titleFormat', 'urlFormat'], (result) => {
+      const restrictedUrls = result.restrictedUrls;
+      const titleFormat = result.titleFormat || '## {title}';
+      const urlFormat = result.urlFormat || '[{url}]({url})';
+  
       chrome.windows.getAll({ populate: true, windowTypes: ['normal'] }, (windows) => {
         const currentWindow = windows.find((window) => window.focused);
-        const selectedTabs = currentWindow.tabs.filter((tab) => tab.highlighted);
-        const processOnlySelectedTabs = selectedTabs.length > 1;
-        const tabs = currentWindow.tabs.filter((tab) => shouldProcessTab(tab, restrictedUrls || [], processOnlySelectedTabs));
+        const tabs = currentWindow.tabs.filter((tab) => shouldProcessTab(tab, restrictedUrls || []));
   
-        const { markdown, formattedTimestamp } = generateMarkdownAndTimestamp(tabs);
+        const { markdown, formattedTimestamp } = generateMarkdownAndTimestamp(tabs, titleFormat, urlFormat);
+  
+        const base64Markdown = btoa(unescape(encodeURIComponent(markdown)));
   
         chrome.downloads.download({
-          url: 'data:text/markdown;charset=UTF-8,' + encodeURIComponent(markdown),
+          url: 'data:text/markdown;charset=UTF-8;base64,' + base64Markdown,
           filename: `${formattedTimestamp}_OpenTabs.md`,
           saveAs: true,
         });
